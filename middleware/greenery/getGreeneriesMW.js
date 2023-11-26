3ae71954c1d063bcc11ab4a1928b71f3f25bfc63
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-11-26 02:32:55 +0100

    greenery count

--- middleware/greenery/getGreeneriesMW.js
+++ middleware/greenery/getGreeneriesMW.js
@@ -5,8 +5,20 @@ module.exports = function (objectrepository) {
 	const GreeneryModel = requireOption(objectrepository, 'GreeneryModel');
   
     return function (req, res, next) {
-        GreeneryModel.find()
-        .sort({ name: 1 })
+        GreeneryModel.aggregate([
+            { $lookup: {
+                from: 'sensorboxes',
+                localField: '_id',
+                foreignField: '_greenery',
+                as: 'sensorboxes'
+            } },
+            { $project: {
+                _id: 1,
+                name: 1,
+                count: { $size: '$sensorboxes' }
+            } },
+            { $sort : { name : 1 } }
+        ])
         .then (function (greeneries) {
             if (!greeneries) {
             	return next();
